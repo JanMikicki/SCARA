3c3e5a8fa50150035609584e44ec343ab5708ab9
JanMikicki <jan.mikicki@outlook.com>
2018-05-31 14:46:01 +0200

    hm

---
+++ src/scara/SCARA.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scara;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+
+import javax.media.j3d.*;
+import javax.vecmath.Vector3f;
+
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.behaviors.mouse.MouseWheelZoom;
+import com.sun.j3d.utils.geometry.Box;
+import com.sun.j3d.utils.geometry.Cylinder;
+import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.image.TextureLoader;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+
+
+public class SCARA extends Applet implements ActionListener, KeyListener {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    public void actionPerformed(ActionEvent e){}
+    
+    public void keyReleased(KeyEvent e){
+        
+    }
+    public void keyTyped(KeyEvent e){}
+    public void keyPressed(KeyEvent e){}
+}
