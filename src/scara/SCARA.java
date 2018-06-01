8619c9bed00593ac61d0b434cc2e8c9d6627fb30
JanMikicki <jan.mikicki@outlook.com>
2018-06-01 15:19:10 +0200

    podstawa i walec

--- src/scara/SCARA.java
+++ src/scara/SCARA.java
@@ -26,34 +26,66 @@ import com.sun.j3d.utils.geometry.*;
 
 public class SCARA extends Applet implements ActionListener, KeyListener {
     
+    private SimpleUniverse u;
+    
+    //konstruktor klasy
     public SCARA(){
     GraphicsConfiguration config =
     SimpleUniverse.getPreferredConfiguration();
     
     Canvas3D canvas3D = new Canvas3D(config);   
-    SimpleUniverse simpleU = new SimpleUniverse(canvas3D);
+    u = new SimpleUniverse(canvas3D);
     
     Transform3D przesuniecie_obserwatora = new Transform3D();
     przesuniecie_obserwatora.set(new Vector3f(0.0f,0.0f,3.0f));
 
-    simpleU.getViewingPlatform().getViewPlatformTransform().setTransform(przesuniecie_obserwatora);
+    u.getViewingPlatform().getViewPlatformTransform().setTransform(przesuniecie_obserwatora);
     setLayout(new BorderLayout());
   
     add(BorderLayout.CENTER, canvas3D);
-    
-    Color3f blop = new Color3f(255,255,255);
-    
-    AmbientLight swiatlo  = new AmbientLight(true, blop);
-    swiatlo.setInfluencingBounds(new BoundingSphere());
-    
-    BranchGroup scena = new BranchGroup();
-    scena.addChild(new ColorCube(0.4));
-    scena.addChild(swiatlo);
-    scena.compile();
-    
-    simpleU.addBranchGraph(scena);
+                
+    BranchGroup scena = stworzenieSceny();
+       
+    u.addBranchGraph(scena);
     
     }
+    
+    private BranchGroup stworzenieSceny(){
+        BranchGroup tworzonaScena = new BranchGroup();
+        
+        AmbientLight swiatlo  = new AmbientLight();
+        swiatlo.setInfluencingBounds(new BoundingSphere());
+        
+        Material material = new Material();
+        material.setAmbientColor(new Color3f(0.4f, 0.2f, 1.0f));
+        
+        Appearance wyglad = new Appearance();
+        wyglad.setColoringAttributes(new ColoringAttributes(0.5f,0.5f,0.9f,ColoringAttributes.NICEST));
+        wyglad.setMaterial(material);
+        
+        /**
+         * Stworzenie podstawy, na której znajduje się robot typu SCARA.
+         */
+        Box podstawa = new Box(0.25f,0.01f,0.25f, wyglad);
+        TransformGroup podstawaTG = new TransformGroup();
+        podstawaTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        podstawaTG.addChild(podstawa);
+         
+        //WALEC
+        Cylinder walec = new Cylinder(0.1f, 0.4f, wyglad);
+        Transform3D poz_walec = new Transform3D();
+        poz_walec.set(new Vector3f(0.0f, 0.2f, 0));
+        TransformGroup przes_walec = new TransformGroup(poz_walec);
+        przes_walec.addChild(walec);
+        podstawaTG.addChild(przes_walec);
+        
+        tworzonaScena.addChild(podstawaTG);
+        tworzonaScena.addChild(swiatlo);
+        tworzonaScena.compile();
+        
+        return tworzonaScena;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
        new MainFrame(new SCARA(), 700, 700);
